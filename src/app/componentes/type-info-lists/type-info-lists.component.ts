e149e53851247e818fe1cba52437a5c225a08d68
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2022-05-08 12:29:10 -0500

    Added styling

--- src/app/componentes/type-info-lists/type-info-lists.component.ts
+++ src/app/componentes/type-info-lists/type-info-lists.component.ts
@@ -11,44 +11,63 @@ import { HttpClient } from '@angular/common/http';
 export class TypeInfoListsComponent implements OnInit {
   pokemonTypeData: PokemonType = this.initData();
   pokemonName!: string;
+  pokemonTypes: string[] = [];
+  pokemonNotFound = false;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   getData() {
-    this.pokemonTypeData = this.initData();
+    if (!this.pokemonName || this.pokemonName.split(' ').join('').length === 0)
+      return;
+
+    this.resetData();
+
+    this.pokemonName = this.pokemonName.trim();
 
     this.http
       .get<Pokemon>(
         `https://pokeapi.co/api/v2/pokemon/${this.pokemonName.toLowerCase()}`
       )
-      .subscribe((pokemonData) => {
-        this.http
-          .get<PokemonType>(pokemonData.types[0].type.url)
-          .subscribe((typeData) => {
-            typeData.damage_relations.double_damage_from.forEach((item) => {
-              this.pokemonTypeData.damage_relations.double_damage_from.push(
-                item
-              );
-            });
-            typeData.damage_relations.double_damage_to.forEach((item) => {
-              this.pokemonTypeData.damage_relations.double_damage_to.push(item);
-            });
-            typeData.damage_relations.half_damage_from.forEach((item) => {
-              this.pokemonTypeData.damage_relations.half_damage_from.push(item);
-            });
-            typeData.damage_relations.half_damage_to.forEach((item) => {
-              this.pokemonTypeData.damage_relations.half_damage_to.push(item);
-            });
-            typeData.damage_relations.no_damage_from.forEach((item) => {
-              this.pokemonTypeData.damage_relations.no_damage_from.push(item);
-            });
-            typeData.damage_relations.no_damage_to.forEach((item) => {
-              this.pokemonTypeData.damage_relations.no_damage_to.push(item);
-            });
+      .subscribe(
+        (pokemonData) => {
+          pokemonData.types.forEach((type) => {
+            this.pokemonTypes.push(type.type.name);
           });
-      });
+          this.http
+            .get<PokemonType>(pokemonData.types[0].type.url)
+            .subscribe((typeData) => {
+              typeData.damage_relations.double_damage_from.forEach((item) => {
+                this.pokemonTypeData.damage_relations.double_damage_from.push(
+                  item
+                );
+              });
+              typeData.damage_relations.double_damage_to.forEach((item) => {
+                this.pokemonTypeData.damage_relations.double_damage_to.push(
+                  item
+                );
+              });
+              typeData.damage_relations.half_damage_from.forEach((item) => {
+                this.pokemonTypeData.damage_relations.half_damage_from.push(
+                  item
+                );
+              });
+              typeData.damage_relations.half_damage_to.forEach((item) => {
+                this.pokemonTypeData.damage_relations.half_damage_to.push(item);
+              });
+              typeData.damage_relations.no_damage_from.forEach((item) => {
+                this.pokemonTypeData.damage_relations.no_damage_from.push(item);
+              });
+              typeData.damage_relations.no_damage_to.forEach((item) => {
+                this.pokemonTypeData.damage_relations.no_damage_to.push(item);
+              });
+            });
+        },
+        () => {
+          this.pokemonNotFound = true;
+        }
+      );
   }
 
   initData() {
@@ -62,5 +81,11 @@ export class TypeInfoListsComponent implements OnInit {
         no_damage_to: [],
       },
     };
+  }
+
+  resetData() {
+    this.pokemonTypeData = this.initData();
+    this.pokemonTypes = [];
+    this.pokemonNotFound = false;
   }
 }
